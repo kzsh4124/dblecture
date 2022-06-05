c8cbc85c2b9438e1649381a926c60d5ecb49d60c
DESKTOP-V63MRH9\kazus <kazushi.02140413@gmail.com>
2022-06-05 22:33:21 +0900

    add seminar.php、index.html

---
+++ html/seminar.php
@@ -0,0 +1,29 @@
+<html>
+    <head><title>seminar.php</title></head>
+    <body>
+        <?php
+        $mysqli = new mysqli('localhost', 's2111609', 'hogehoge', 's2111609');
+        if ($mysqli->connect_error) {
+            echo $mysqli->connect_error;
+            exit();
+        } else {
+            $mysqli->set_charset("utf8");
+        }
+        $sql = "SELECT * FROM seminar";
+        $res = $mysqli->query($sql);
+        print("<tr>");
+        for( $i = 0; $i < $res->field_count; $i++ ){
+            print( "<td>".$res->fetch_field_direct($i)->name."</td>" );
+        }
+        print("</tr>");
+        while($row = $res->fetch_array()) {
+            print("<tr>");
+            for( $i = 0; $i < $res->field_count; $i++ ){
+            print( "<td>".$row[$i]."</td>" );
+        }
+        print("</tr>");
+        }
+        ?>
+    <a href="index.html">トップへ戻る</a>
+    </body>
+</html>
--- test-week2/init.sql
+++ test-week2/init.sql
@@ -1,9 +1,24 @@
-insert into status (0,'休止中/準備中');
+insert into status (0,'準備中');
 insert into status (1,'開催中');
-insert into status (2,'終了');
+insert into status (2,'休止中');
+insert into status (3,'終了');
 
 insert into type (0,'輪読');
 insert into type (1,'勉強会');
 insert into type (2,'講義');
 
-
+insert into subject(0,'情報科学')
+insert into subject(1,'文学')
+insert into subject(2,'言語系')
+insert into subject(3,'社会学')
+insert into subject(4,'数学')
+insert into subject(5,'天文学')
+insert into subject(6,'物理学')
+insert into subject(7,'地学')
+insert into subject(8,'化学')
+insert into subject(9,'工学')
+insert into subject(10,'生物学')
+insert into subject(11,'医学')
+insert into subject(12,'獣医学')
+insert into subject(13,'音楽')
+insert into subject(14,'その他')
