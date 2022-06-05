9c54d4a943252d1a7d9d2f05fd63400dea7094c7
DESKTOP-V63MRH9\kazus <kazushi.02140413@gmail.com>
2022-06-05 22:50:43 +0900

    fix init.sql

--- test-week2/init.sql
+++ test-week2/init.sql
@@ -1,24 +1,24 @@
-insert into status (0,'準備中');
-insert into status (1,'開催中');
-insert into status (2,'休止中');
-insert into status (3,'終了');
+insert into status values(0,'準備中');
+insert into status values(1,'開催中');
+insert into status values(2,'休止中');
+insert into status values(3,'終了');
 
-insert into type (0,'輪読');
-insert into type (1,'勉強会');
-insert into type (2,'講義');
+insert into type values(0,'輪読');
+insert into type values(1,'勉強会');
+insert into type values(2,'講義');
 
-insert into subject(0,'情報科学')
-insert into subject(1,'文学')
-insert into subject(2,'言語系')
-insert into subject(3,'社会学')
-insert into subject(4,'数学')
-insert into subject(5,'天文学')
-insert into subject(6,'物理学')
-insert into subject(7,'地学')
-insert into subject(8,'化学')
-insert into subject(9,'工学')
-insert into subject(10,'生物学')
-insert into subject(11,'医学')
-insert into subject(12,'獣医学')
-insert into subject(13,'音楽')
-insert into subject(14,'その他')
+insert into subject values(0,'情報科学')
+insert into subject values(1,'文学')
+insert into subject values(2,'言語系')
+insert into subject values(3,'社会学')
+insert into subject values(4,'数学')
+insert into subject values(5,'天文学')
+insert into subject values(6,'物理学')
+insert into subject values(7,'地学')
+insert into subject values(8,'化学')
+insert into subject values(9,'工学')
+insert into subject values(10,'生物学')
+insert into subject values(11,'医学')
+insert into subject values(12,'獣医学')
+insert into subject values(13,'音楽')
+insert into subject values(14,'その他')
