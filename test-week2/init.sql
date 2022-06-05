38fc947062a5739dc543bfc73421dd39ab40c0ef
s2111609 <s2111609@turkey.slis.tsukuba.ac.jp>
2022-06-05 15:50:46 +0900

    index.html

---
+++ test-week2/init.sql
@@ -0,0 +1,9 @@
+insert into status (0,'休止中/準備中');
+insert into status (1,'開催中');
+insert into status (2,'終了');
+
+insert into type (0,'輪読');
+insert into type (1,'勉強会');
+insert into type (2,'講義');
+
+
---
+++ test-week2/schme.sql
@@ -0,0 +1,44 @@
+create table seminar(
+id int auto_increment primary key,
+name varchar(40) not null,
+status int not null,
+subject int,
+difficulty int,
+day int not null,
+start_time time,
+type int
+);
+
+create table status(
+status_id int primary key,
+name varchar(20)
+);
+
+create table type(
+type_id int primary key,
+name varchar(30)
+);
+
+create table subject(
+subject_id int primary key,
+name varchar(30)
+);
+
+create table keyword(
+word_id int auto_increment primary key,
+keyword varchar(40),
+seminar_id int
+);
+
+crete table member(
+member_id int auto_increment primary key,
+name varchar(30)
+);
+
+create table belong(
+seminar_id int,
+member_id int,
+is_organizer bit,
+primary key(seminar_id, member_id)
+);
+
