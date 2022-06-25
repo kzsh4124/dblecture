b87a77e1abacdce2acb29cb9d5045d5eaaed5e33
DESKTOP-V63MRH9\kazus <kazushi.02140413@gmail.com>
2022-06-26 00:32:56 +0900

    insertの実装

---
+++ html/insert_order.php
@@ -0,0 +1,41 @@
+<?php
+
+try{
+    if(isset($_POST['title'])){
+        $title = $_POST['title'];
+    }
+    if(isset($_POST['subject'])){
+        $subject = $_POST['subject'];
+    }
+    if(isset($_POST['status'])){
+        $status = $_POST['status'];
+
+    }
+    if(isset($_POST['day'])){
+        $day = $_POST['day'];
+    }
+    if(isset($_POST['time'])){
+        $time = $_POST['time'];
+    }else{
+        $time='';
+    }
+    if(isset($_POST['content'])){
+        $content = $_POST['content'];
+    }else{
+        $content = '';
+    }
+    $values = [$title, $subject, $status, $day, $time, $content];
+
+    $pdo = new PDO('mysql:dbname=s2111609;host=localhost;charset=utf8mb4', 's2111609', 'hogehoge', [
+        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+        PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+    ]);
+    $pdo->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
+    $sql = "INSERT INTO seminar (name, subject, status, day, start_time, content, approval) values (?,?,?,?,?,?,0)";
+    $stmt = $pdo->prepare($sql);
+    $stmt->execute($values);
+
+}catch(PDOException $e){
+    header('Content-Type: text/plain; charset=UTF-8', true, 500);
+    exit($e->getMessage());
+}
